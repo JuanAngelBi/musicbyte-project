6e1d640fb8197a38492feab2e60fbe17db33f4ea
JuanAngelBi <105744962+JuanAngelBi@users.noreply.github.com>
2023-06-06 01:58:15 -0300

    Rutas Ordenadas

--- ingreso.php
+++ ingreso.php
@@ -1,16 +1,13 @@
 <?php
-
     session_start();
-
-    if(isset($_SESSION['usuario'])){
+    if (isset($_SESSION['usuario'])) {
         header("location: musicbyte.php");
     }
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -21,6 +18,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/estilos.css">
 </head>
+
 <body>
     <main>
         <!-- Caja principal -->
@@ -31,35 +29,36 @@
                 <div class="caja__trasera-login">
                     <h3>¿Ya tienes una cuenta?</h3>
                     <p>¡Ingresa ahora!</p>
-                    <button id="btn__iniciar-sesion" >Iniciar Sesión</button>
+                    <button id="btn__iniciar-sesion">Iniciar Sesión</button>
                 </div>
                 <!-- Caja de registro -->
                 <div class="caja__trasera-register">
                     <h3>¿Aún no tienes una cuenta?</h3>
                     <p>¡Registrate ahora!</p>
-                    <button id="btn__registrarse" >Registrarse</button>
+                    <button id="btn__registrarse">Registrarse</button>
                 </div>
             </div>
             <!-- Formularios de Login y Registro -->
             <div class="contenedor__login-register">
                 <!-- Login -->
-                 <form action="php/login_usuario_be.php" method="POST" class="formulario__login">
+                <form action="php/login_usuario_be.php" method="POST" class="formulario__login">
                     <h2>Iniciar Sesión</h2>
                     <input type="text" placeholder="Correo electrónico" name="correo">
                     <input type="password" placeholder="Contraseña" name="contrasena">
                     <button>Entrar</button>
-                 </form> 
-                 <!-- Registro -->
-                 <form action="php/registro_usuario_be.php" method="POST" class="formulario__register">
+                </form>
+                <!-- Registro -->
+                <form action="php/registro_usuario_be.php" method="POST" class="formulario__register">
                     <h2>Registrarse</h2>
                     <input type="email" placeholder="Correo electrónico" name="correo">
                     <input type="text" placeholder="Nombre de usuario" name="nombre_usuario">
                     <input type="password" placeholder="Contraseña" name="contrasena">
                     <button>Registrarse</button>
-                 </form>
+                </form>
             </div>
         </div>
     </main>
     <script src="assets/js/script.js"></script>
 </body>
+
 </html>
